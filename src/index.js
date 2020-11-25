b679e3a543e6413c8ea6f39d05a9c3ebca02ef56
Viktoriia Strelnikova <v.strelnikova91@gmail.com>
2020-11-26 00:33:22 +0200

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./css/styles.css";
-import "./js/gallery-items";
-import "./js/gallery-creation";
-import "./js/open-modal";
-import "./js/close-modal";
-import "./js/native-slider";
-import "./js/lazy-load-test";
--- src/js/refs-data-module.js
+++
@@ -1,5 +0,0 @@
-export const modalContainer = document.querySelector('.js-gallery');
-export const openModal = document.querySelector('.js-lightbox');
-export const changeImage = document.querySelector('.lightbox__image');
-export const closeModal = document.querySelector('[data-action="close-lightbox"]');
-export const modalOverlay = document.querySelector('.lightbox__overlay');
